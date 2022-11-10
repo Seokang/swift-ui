6c13ce11418486771d18aa1030eb06039f262e91
SeoKang <seokang@dnx.kr>
2022-11-10 21:44:40 +0900

    Initial Commit

---
+++ swift-ui/swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_uiApp.swift
+//  swift-ui
+//
+//  Created by SeoKang on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
